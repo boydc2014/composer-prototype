ef8c5f54cca9612677e7182358477a4bc6cbb4dc
Dong Lei <donglei@microsoft.com>
2019-01-31 23:13:23 +0800

    make the sample real

--- Composer/src/config.js
+++ Composer/src/config.js
@@ -1,6 +1,11 @@
+
+// this is currently hard-coded here
+// it should be discovered and generated when compiling things together
+
 const EditorMapping = {
     '.bot' : 'static/extensions/jsonEditor.html',
     '.json': 'static/extensions/jsonEditor.html',
+    '.lg': 'static/extensions/markdownEditor.html',
     '.lu': 'static/extensions/markdownEditor.html',
     '.md': 'static/extensions/markdownEditor.html'
 }
